a0f0d1b931a7346a4525f33d021c4959d711973b
Rairocha <52248306+Rairocha@users.noreply.github.com>
2023-06-15 18:53:38 -0300

    solution

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,22 +5,36 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    return this.stackControl.length<this.MAX_SIZE
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.stackControl.length==0
   }
 
   push(item) {
+    if(this.canPush()){
+      this.stackControl.push(item)
+      return this.stackControl
+    }
+    else{
+      throw new Error('STACK_OVERFLOW');
+    }
     // ... your code goes here
   }
 
   pop() {
+    if(!this.isEmpty()){
+      return this.stackControl.pop()
+    }
+    else{
+      throw new Error('STACK_UNDERFLOW');
+    }
     // ... your code goes here
   }
 
   display() {
+    return this.stackControl
     // ... your code goes here
   }  
 }
